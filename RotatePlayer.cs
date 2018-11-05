a79f8d6b34474ddd477012fae7f97d01ce74bf7b
PoorPlanning <44105171+PoorPlanning@users.noreply.github.com>
2018-11-04 23:28:47 -0800

    Add files via upload

---
+++ RotatePlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotatePlayer : MonoBehaviour {
+    public float speed = 5f;
+
+    private void Update() {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+}
