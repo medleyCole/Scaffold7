9589b2fb78ec2b4fad1a387f10ab765c38674ffb
Benjamin <bkeyro99@gmail.com>
2018-11-07 12:31:02 -0800

    Created an enemy spawner

---
+++ Top Down/Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour{
+
+    public GameObject enemy;
+    public Transform[] spawnspots;
+    private float timeBtwSpawns;
+    public float startTimeBtwSpawns;
+
+    private void Start()
+    {
+        timeBtwSpawns = startTimeBtwSpawns;
+    }
+    void Update(){
+        if(timeBtwSpawns <= 0)
+        {
+            int randPos = UnityEngine.Random.Range(0, spawnspots.Length-1);
+            Instantiate(enemy, spawnspots[randPos].position, Quaternion.identity);
+            timeBtwSpawns = startTimeBtwSpawns;
+        }
+        else
+        {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+    }
+
+    private void Instantiate(GameObject enemy, Transform transform, Quaternion identity)
+    {
+        throw new NotImplementedException();
+    }
+}
