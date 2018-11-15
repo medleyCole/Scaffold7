34512fc011a2c9a6cce698e5b0ce8758a0eac133
Brian Lam <BrianLam4101@gmail.com>
2018-11-15 01:18:08 -0800

    Lights Change Size to Feel More Alive
    Lights grow and shrink over time
    - Lights slowly expand and shrink
    - Lights will occasionally flicker with a sudden burst of area then shrink back

---
+++ Assets/LightFlicker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+    public Transform lightMasks;
+    public float chanceToFlicker;
+    public float minFlickerScale;
+    public float maxFlickerScale;
+    public float minFlickerDuration;
+    public float maxFlickerDuration;
+    public float minVarianceScale;
+    public float maxVarianceScale;
+    public float varianceSpeed;
+    public float varianceUpdateRate;
+    private Vector3 scaleVelocity;
+    private Vector3 targetVariance;
+
+    private void Start() {
+        StartCoroutine(UpdateVariance(varianceUpdateRate));
+    }
+
+    private void Update() {
+        if (Mathf.Pow(1 + chanceToFlicker, Time.deltaTime) - 1 > Random.Range(0, 1f))
+            StartCoroutine(Flicker(Vector3.one * Random.Range(minFlickerScale, maxFlickerScale), Random.Range(minFlickerDuration, maxFlickerDuration)));
+        lightMasks.localScale = Vector3.SmoothDamp(lightMasks.localScale, targetVariance, ref scaleVelocity, varianceSpeed);
+    }
+
+    private IEnumerator UpdateVariance(float updateRate) {
+        var timer = new WaitForSeconds(updateRate);
+        while (true) {
+            targetVariance = Vector3.one * Random.Range(minVarianceScale, maxVarianceScale);
+            yield return timer;
+        }
+    }
+
+    private IEnumerator Flicker(Vector3 scale, float duration) {
+        Vector3 originalScale = lightMasks.transform.localScale;
+        lightMasks.localScale = scale;
+        float endTime = Time.time + duration;
+        while (endTime > Time.time)
+            yield return null;
+        lightMasks.localScale = originalScale;
+    }
+}
